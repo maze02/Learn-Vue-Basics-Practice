a82783b0f996a570040d417e9ca13e812c3e7737
maze02 <mariasb2@gmail.com>
2021-10-27 22:21:39 +0200

    list rendering

--- shopping.js
+++ shopping.js
@@ -4,7 +4,12 @@ const app = new Vue ({
         product: 'Socks', 
         description: 'Green sock with red frogs',
         image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-        link:"https://www.google.com/search?q=socks&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjFs7WtmevzAhWfA2MBHaRrBLUQ_AUoAXoECAEQAw&biw=1536&bih=763&dpr=1.25"
+        link:"https://www.google.com/search?q=socks&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjFs7WtmevzAhWfA2MBHaRrBLUQ_AUoAXoECAEQAw&biw=1536&bih=763&dpr=1.25",
+        inStock: false,
+        inventory: 10,
+        onSale: true,
+        details:["80% cotton", "20% polyester", "Gender-neutral"],
+        sizes:["XL", "L", "M", "S", "XS"]
     }
 });
 
