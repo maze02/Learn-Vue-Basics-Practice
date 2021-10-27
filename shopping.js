c4aa3fefe87057e9dc87bfde7c930f690322075f
maze02 <mariasb2@gmail.com>
2021-10-27 20:21:01 +0200

    Lesson 2 Complete

--- shopping.js
+++ shopping.js
@@ -2,8 +2,10 @@ const app = new Vue ({
     el:'#app',
     data:{
         product: 'Socks', 
-        description: 'Green with red frogs'
+        description: 'Green sock with red frogs',
+        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        link:"https://www.google.com/search?q=socks&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjFs7WtmevzAhWfA2MBHaRrBLUQ_AUoAXoECAEQAw&biw=1536&bih=763&dpr=1.25"
     }
 });
 
-app.product = "I've changed the data";
+
